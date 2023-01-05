9984538702971c0c9d1a209b513f0acab030c833
Ralph Odevilas <rdcodevilas@gmail.com>
2023-01-06 02:13:36 +0800

    initial commit

---
+++ src/pages/FinishedTasks.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+import { FinishedTasks } from "../composables/Tasks";
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <div class="full-width q-pa-xl">
+      <h6 class="q-my-none q-mb-md">Finished Tasks</h6>
+      <div class="q-mt-xl">
+        <q-card v-for="row in FinishedTasks" :key="row.id" class="q-mt-sm">
+          <q-card-section class="bg-primary text-white q-pa-none">
+            <div class="text-bold q-pa-md">{{ row.todo }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </div>
+</template>
